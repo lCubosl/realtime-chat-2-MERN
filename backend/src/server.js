0035a31a92dd10bed8c21d9f777a04aa19a19de9
shifter <muislarques@gmail.com>
2025-06-03 23:28:47 +0100

    User schema for mongobd database, hashing with bcrypt

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/bd.js"
 
 dotenv.config()
 
@@ -12,4 +13,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}: http://localhost:5001`)
+  connectDB()
 })
