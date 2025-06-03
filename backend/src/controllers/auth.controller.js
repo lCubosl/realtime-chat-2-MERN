0acddbe532f78afd0cd95769d86d6dbe7f1524db
shifter <muislarques@gmail.com>
2025-06-03 19:50:58 +0100

    routes logic to auth.controller.js,

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req, res) {
+  res.send("Signup Route")
+}
+
+export async function login(req, res) {
+  res.send("Lgoin Route")
+}
+
+export async function logout(req, res) {
+  res.send("Logout Route")
+}
