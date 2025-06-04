b38b0d8bbbf277cc262557e75e7e93738c107031
shifter <muislarques@gmail.com>
2025-06-04 17:26:34 +0100

    signup route now updates database. Tests via postman agent

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -48,8 +48,6 @@ const userSchema = new mongoose.Schema({
   ],
 }, {timestamps:true})
 
-const User = mongoose.model("User", userSchema)
-
 userSchema.pre("save", async function (next) {
   if(!this.isModified("password")) return next()
   // hashes password
@@ -62,5 +60,7 @@ userSchema.pre("save", async function (next) {
     next(error)
   }
 })
+
+const User = mongoose.model("User", userSchema)
 
 export default User
