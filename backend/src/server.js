4bf7ae126d0fab285f26966a1f000bce9fcf7d12
LuisM <muislarques@gmail.com>
2025-06-13 11:24:02 +0100

    new: setup redirects, understanding tanstack

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
+import cors from "cors"
+
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
@@ -16,6 +18,7 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
 
+app.use(cors({origin:"http://localhost:5173", credentials:"true"}))
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
