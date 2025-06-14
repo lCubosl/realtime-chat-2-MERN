1cfaca75a242b7ed02a3fe95c6aed4524515337b
LuisM <muislarques@gmail.com>
2025-06-14 17:37:10 +0100

    new: themeselector for the navbar. all other new components are related to it.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,9 +12,12 @@ import { Toaster } from "react-hot-toast";
 import PageLoader from "./components/PageLoader.jsx";
 import useAuthUser from "./hooks/useAuthUser.js";
 import Layout from "./components/Layout.jsx";
+import { useThemeStore } from "./store/useThemeStore.js";
 
 const App = () => {
   const {isLoading, authUser} = useAuthUser()
+
+  const {theme} = useThemeStore()
 
   const isAuthenticated = Boolean(authUser)
   const isOnboarded = authUser?.isOnboarded
@@ -22,7 +25,7 @@ const App = () => {
   if(isLoading) return <PageLoader />
 
   return (
-    <div className="h-screen" data-theme="night">
+    <div className="h-screen" data-theme={theme}>
       <Routes>
         <Route
           path="/"
